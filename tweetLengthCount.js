b4acab73140f251f5a799d1277a088c158804404
Omkar Patole <omkarpatole1799@gmail.com>
2022-05-23 07:31:36 +0530

    added comments

--- tweetLengthCount.js
+++ tweetLengthCount.js
@@ -1,4 +1,9 @@
+//taking user input
 var tweet = prompt("Tweet Here!");
+
+//counting length of tweet using .length function
 var tweetLength = tweet.length;
+
+//printing the length of tweet and remaining characters
 alert("You have written " + tweetLength + " characters, you are left with " 
 + (140-tweetLength) + " characters!");
