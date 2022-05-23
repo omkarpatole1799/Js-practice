ed9ffed804bbe89808366d1ef1cfc7a89c498a3d
Omkar Patole <omkarpatole1799@gmail.com>
2022-05-23 07:28:37 +0530

    init

---
+++ tweetLengthCount.js
@@ -0,0 +1,4 @@
+var tweet = prompt("Tweet Here!");
+var tweetLength = tweet.length;
+alert("You have written " + tweetLength + " characters, you are left with " 
++ (140-tweetLength) + " characters!");
