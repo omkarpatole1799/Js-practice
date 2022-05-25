26190132b4cfe0b6cd410e4056a3c7122f94c326
Omkar Patole <omkarpatole1799@gmail.com>
2022-05-25 07:23:35 +0530

    dogAgeToHumanAgeCalculator

--- dogAgeToHumanAgeCalculator.js
+++ dogAgeToHumanAgeCalculator.js
@@ -2,7 +2,7 @@
 let dogAge = prompt("What is Dog Age: " );
 
 //calculating with foremula
-humanAge = (dogAge-2)*4+21;
+humanAge = ((dogAge-2)*4)+21;
 
 //giving alert for the output
-alert(humanAge);
+alert("Your dog is: " + humanAge + " years old in human years");
