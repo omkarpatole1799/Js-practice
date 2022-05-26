ddc48f60d90921af4014aabc34ca88ab6bdfce01
Omkar Patole <omkarpatole1799@gmail.com>
2022-05-26 08:22:19 +0530

    init

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,7 +1,20 @@
 //this code is for bmi calculator
 
 function bmiCalcualte(weight,height){
-    return (weight/(height*height));
+
+    let bmi = (weight/(height*height));
+
+    //various conditions
+    if (bmi<18.5){
+        alert("Your BMI is: " + bmi + ", so you are underwright.");
+    }
+    if (bmi < 18.5 && bmi >24.9) {
+        alert("Your BMI is: " + bmi + ", so you have normal weight.");
+    }
+    if (bmi > 24.9) {
+        alert("Your BMI is: " + bmi + ", so you are overweight.");
+    }
+    return bmi;  
 }
 
 //weight should be in kg
@@ -10,5 +23,3 @@ function bmiCalcualte(weight,height){
 //calling the function
 var bmi= Math.round(bmiCalcualte(65,1.9));
 
-//printing the output
-console.log("Your bmi is: " + bmi);
