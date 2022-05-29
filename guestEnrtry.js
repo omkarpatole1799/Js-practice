377bab19840251d8120b3b07b1764f5e9e528d07
Omkar Patole <omkarpatole1799@gmail.com>
2022-05-29 06:52:33 +0530

    guest entry program

---
+++ guestEnrtry.js
@@ -0,0 +1,8 @@
+var guestName = prompt("What is your name: ");
+var guestList = ["Angela" , "Jhon", "Ankur", "Jim"];
+
+if (guestList.includes(guestName)) {
+    alert("Welcome");
+} else {
+    alert("Sorry, next time!");
+}
