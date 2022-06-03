6444c640af3bb4a4f534c60a3139045cdd975652
Omkar Patole <omkarpatole1799@gmail.com>
2022-06-03 07:15:49 +0530

    added fibonacchi sequence code

---
+++ JS/fibonacchiGenerator.js
@@ -0,0 +1,26 @@
+console.log("Welcome to fibonacchi generator");
+
+
+function fibonacchiGenerator(number) {
+    var a = 0;
+    var b = 1;
+
+    if (number == 1) {
+        console.log(a);
+        
+    } else {
+        console.log(a);
+        console.log(b);
+    }
+
+    for (var i = 2; i<=number; i++){
+        c = a + b;
+        a = b;
+        b = c;
+        console.log(c);
+
+    }
+
+}
+
+ 
