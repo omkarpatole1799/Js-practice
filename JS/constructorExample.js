0fcf238a0d71548581bf7ca7db38ce911181bb66
Omkar Patole <omkarpatole1799@gmail.com>
2022-07-10 10:55:28 +0530

    init

---
+++ JS/constructorExample.js
@@ -0,0 +1,10 @@
+
+
+// constructor for housekeeper
+var HouseKeeper (age, name, experience, work ) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+    this.work = work;
+}
+
